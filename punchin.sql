13e3347da99a0619181b81a0a0a061f5692dce0a
Saurabh Shukla <71999030+100rabhShukla@users.noreply.github.com>
2021-11-24 14:12:29 +0530

    Add files via upload

---
+++ punchin.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS [punchin1] (
+[name] VARCHAR NULL,
+[punchin2] INT NULL,
+[punchout2] INT NULL
+);
+
+INSERT INTO punchin1 VALUES
+('keyur',9,19),
+('saurabh',10,20),
+('krunal',9,22),
+('jaimin',8,17);
